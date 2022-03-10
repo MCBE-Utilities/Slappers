25f0213d379546a5cd0a3d7d710e88f20613c6b6
PMK744 <69542280+PMK744@users.noreply.github.com>
2022-03-09 20:24:37 -0600

    added effect

--- src/listeners/Effects.ts
+++ src/listeners/Effects.ts
@@ -6,6 +6,7 @@ client.on('Tick', () => {
         const entity = client.entities.getByUniqueId(slapper.uniqueId)
         if (!entity) continue
         entity.executeCommand('tp @s ~ ~ ~')
-        entity.addEffect(Minecraft.MinecraftEffectTypes.instantHealth, 20, 255)
+        entity.executeCommand('effect @s instant_health 20 255 true')
+        entity.executeCommand('effect @s weakness 20 255 true')
     }
 })
